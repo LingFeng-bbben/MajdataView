cc2f7c6f3f8c50db6cacec9450ea9738ea43f22c
LingFeng-bbben <bengy@126.com>
2021-07-23 17:42:44 +0800

    时间显示和ui改进

---
+++ Assets/TImeDisplayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TImeDisplayer : MonoBehaviour
+{
+    Text text;
+    AudioTimeProvider timeProvider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        timeProvider = GameObject.Find("AudioTimeProvider").GetComponent<AudioTimeProvider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int minute = (int)timeProvider.AudioTime / 60;
+        int second = (int)timeProvider.AudioTime - (60 * minute);
+        double mili = (timeProvider.AudioTime - (int)timeProvider.AudioTime) * 10000;
+        text.text = string.Format("{0}:{1:00}.{2:0000}", minute, second, mili);
+    }
+}
