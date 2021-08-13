576469badaea3f7e6fee0e8d74709d6133795b67
LingFeng-bbben <bengy@126.com>
2021-08-13 19:25:29 +0800

    改ui

---
+++ Assets/Scripts/Floating.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Floating : MonoBehaviour
+{
+    public float rotate;
+    public float wx;
+    public float wy;
+    Vector3 startPos;
+    RectTransform rectTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        startPos = rectTransform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform.Rotate(new Vector3(0f, 0f, rotate));
+        rectTransform.localPosition = startPos + new Vector3(Mathf.Sin(Time.realtimeSinceStartup * wx), Mathf.Sin(Time.realtimeSinceStartup * wy));
+    }
+}
