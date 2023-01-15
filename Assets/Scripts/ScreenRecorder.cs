a01b2bd076ee03f30d377220769aa214cf5c6987
LingFeng-bbben <bengy@126.com>
2023-01-15 16:45:12 +0800

    完善录屏，通过pipe输出ffmpeg

--- Assets/Scripts/ScreenRecorder.cs
+++ Assets/Scripts/ScreenRecorder.cs
@@ -1,6 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
 using UnityEngine;
+using System.Text;
+using System.Linq;
 
 public class ScreenRecorder : MonoBehaviour
 {
@@ -14,16 +20,38 @@ public class ScreenRecorder : MonoBehaviour
         StopCoroutine(CaptureScreen());
     }
 
+    private const int BUFFERSIZE = 1920*1080 * 4;
+
     IEnumerator CaptureScreen()
     {
-        while (true)
+        yield return new WaitForEndOfFrame();
+        var texture = ScreenCapture.CaptureScreenshotAsTexture();
+        byte[] data = texture.GetRawTextureData();
+        using (NamedPipeServerStream pipeServer = 
+            new NamedPipeServerStream("test111sb", PipeDirection.Out))
         {
-            yield return new WaitForEndOfFrame();
+            pipeServer.WaitForConnection();
+            print("pipe started");
+            using (BinaryWriter bw = new BinaryWriter(pipeServer))
+            {
+                do
+                {
+                    yield return new WaitForEndOfFrame();
+                    texture.Reinitialize(0, 0);
+                    texture = ScreenCapture.CaptureScreenshotAsTexture();
+/*                    int width = texture.width;
+                    int height = texture.height;*/
+                    
+                    data = texture.GetRawTextureData();
 
-            var texture = ScreenCapture.CaptureScreenshotAsTexture();
-            print(texture.format);
-            //texture.GetRawTextureData
+                    bw.Write(data,0,data.Length);
+                    bw.Flush();
+                    //Thread.Sleep(100);
+                }
+                while (pipeServer.IsConnected);
+            }
         }
+
     }
 
     // Start is called before the first frame update
