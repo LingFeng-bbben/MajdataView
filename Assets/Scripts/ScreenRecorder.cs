a9d20cc88a3f0a57771e14699c143aff9d7adaaa
LingFeng-bbben <bengy@126.com>
2023-01-21 00:24:11 +0800

    修复分辨率问题
    x264编码的分辨率只支持偶数！！

--- Assets/Scripts/ScreenRecorder.cs
+++ Assets/Scripts/ScreenRecorder.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 using System.Text;
 using System.Linq;
 using System.Diagnostics;
+using UnityEngine.UI;
 
 public class ScreenRecorder : MonoBehaviour
 {
@@ -22,44 +23,55 @@ public class ScreenRecorder : MonoBehaviour
         isRecording = false;
     }
 
-    private const int BUFFERSIZE = 1920*1080 * 4;
+   
     bool isRecording = false;
     IEnumerator CaptureScreen(string maidata_path)
     {
+        Screen.SetResolution(1920, 1080, false);
         yield return new WaitForEndOfFrame();
         var texture = ScreenCapture.CaptureScreenshotAsTexture();
         byte[] data = texture.GetRawTextureData();
+        print(texture.width + "x" + texture.height);
+        int BUFFERSIZE = texture.width * texture.height * 4;
         using (NamedPipeServerStream pipeServer = 
             new NamedPipeServerStream("majdataRec", PipeDirection.Out))
         {
             var wavpath = maidata_path + "/out.wav";
             var outputfile = maidata_path + "/out.mp4";
-            var arguments = string.Format(@"-y -f rawvideo -vcodec rawvideo -pix_fmt rgba -s 1920x1080 -r 60 -i \\.\pipe\majdataRec -i {0} -vf {1} -c:v libx264 -preset fast -pix_fmt yuv422p -c:a aac {2}", wavpath, "\"vflip\"", outputfile);
+            var arguments = string.Format(@"-y -f rawvideo -vcodec rawvideo -pix_fmt rgba -s {3}x{4} -r 60 -i \\.\pipe\majdataRec -i {0} -vf {1} -c:v libx264 -preset fast -pix_fmt yuv422p -c:a aac {2}",
+                wavpath, "\"vflip\"", outputfile , texture.width, texture.height);
             var startinfo = new ProcessStartInfo(Application.streamingAssetsPath + "/ffmpeg.exe", arguments);
-            Process.Start(startinfo);
+            startinfo.WorkingDirectory = maidata_path;
+            print(arguments);
+            
+            var p = Process.Start(startinfo);
             pipeServer.WaitForConnection();
-            print("pipe started");
             isRecording = true;
             using (BinaryWriter bw = new BinaryWriter(pipeServer))
             {
                 do
                 {
                     yield return new WaitForEndOfFrame();
-                    texture.Reinitialize(0, 0);
-                    texture = ScreenCapture.CaptureScreenshotAsTexture();
-/*                    int width = texture.width;
-                    int height = texture.height;*/
-                    
-                    data = texture.GetRawTextureData();
+                    try
+                    {
+                        texture.Reinitialize(0, 0);
+                        texture = ScreenCapture.CaptureScreenshotAsTexture();
+                        /*                    int width = texture.width;
+                                            int height = texture.height;*/
 
-                    bw.Write(data,0,data.Length);
-                    bw.Flush();
-                    //Thread.Sleep(100);
+                        data = texture.GetRawTextureData();
+
+                        bw.Write(data, 0, data.Length);
+                        bw.Flush();
+                        //Thread.Sleep(100);
+                    }
+                    catch { }
                 }
                 while (pipeServer.IsConnected && isRecording);
             }
+            //var output = p.StandardError.ReadToEnd();
+            //GameObject.Find("ErrText").GetComponent<Text>().text = output.Substring(output.Length - 500);
         }
-       
     }
 
     // Start is called before the first frame update
